410e0cc48d4ec94ed045c61359697c9278dc4c5a
Radicitus <novivolatus@gmail.com>
2024-06-28 18:36:49 -0400

    Add custom textures

--- src/App.js
+++ src/App.js
@@ -93,10 +93,11 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
   const { nodes, materials } = useGLTF(
     "https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/5huRVDzcoDwnbgrKUo1Lzs/53b6dd7d6b4ffcdbd338fa60265949e1/tag.glb",
   );
-  const texture = useTexture(
-    "https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/SOT1hmCesOHxEYxL7vkoZ/c57b29c85912047c414311723320c16b/band.jpg",
-  );
-  const badgeTexture = useTexture("vercel-ship-badge-texture.jpg");
+  // const texture = useTexture(
+  //   "https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/SOT1hmCesOHxEYxL7vkoZ/c57b29c85912047c414311723320c16b/band.jpg",
+  // );
+  const badgeTexture = useTexture("cam-badge-texture.png");
+  const bandTexture = useTexture("cam-band-texture.png");
   const { width, height } = useThree((state) => state.size);
   const [curve] = useState(
     () =>
@@ -164,7 +165,7 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
   });
 
   curve.curveType = "chordal";
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  bandTexture.wrapS = bandTexture.wrapT = THREE.RepeatWrapping;
 
   function BadgeTexture({ user }) {
     return (
@@ -280,7 +281,7 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
           depthTest={false}
           resolution={[width, height]}
           useMap
-          map={texture}
+          map={bandTexture}
           repeat={[-3, 1]}
           lineWidth={1}
         />
